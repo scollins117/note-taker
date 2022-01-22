8f9423287911e99f7e01ec2d568ca28ed6d61b36
unknown <seancollins117@gmail.com>
2022-01-22 09:16:38 -0800

    trying something

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 
+const { createNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+    
+});
+
 module.exports = router;
