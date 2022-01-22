3183e637c8bd098e2544d407d84889d7b1be939c
unknown <seancollins117@gmail.com>
2022-01-22 12:37:06 -0800

    createNote is not a function

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-function createNote(body, noteArray) {
+function createNote(body, notesArray) {
     const note = body;
-    noteArray.push(note);
+    notesArray.push(note);
     fs.writeFileSync(
-        path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes: noteArray }, null, 2)
+      path.join(__dirname, '../db/db.json'),
+      JSON.stringify({ notesArray }, null, 2)
     );
-
     return note;
-}
+  }
 
 module.export = {
     createNote
--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 
 const { createNote } = require('../../lib/notes');
-const { notes } = require('../../db/db.json');
+const { notesArray } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
     
+});
+
+router.post('/notes', (req, res) => {
+    if(notesArray){
+    req.body.id = notesArray.length.toString();
+    } else 
+    {req.body.id = 0}
+    res.json(createNote(req.body, notesArray));
 });
 
 module.exports = router;
